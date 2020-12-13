82ea05469fa15bd131d437433cd67d96655aa012
Harsha-creator <55666014+Harsha-creator@users.noreply.github.com>
2020-12-13 15:45:19 +0530

    Update Question 2.py

--- Question 2.py
+++ Question 2.py
@@ -2,11 +2,11 @@ my_tuple=(1,2,3,3,5,50,50,5)
 result = tuple(set(my_tuple))
 res = sum(list(result)) 
 def sum(res):
-    if tup == ():
+    if res == ():
         return 0
-    if isinstance(tup[0],int):
+    if isinstance(res[0],int):
         return res[0] + sum(res[1:])
-    if isinstance(tup[0],tuple):
+    if isinstance(res[0],tuple):
         return sum(res[0]) + sum(res[1:])
 totalsum= sum(res)
 print(sum)
